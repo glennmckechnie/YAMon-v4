3e368eb05887853695f36cc66c410962152f5bfc
Glenn <glennmckechnie@users.noreply.github.com>
2026-06-11 20:32:20 +1000

    Add required (for local install) intro *.php messages

---
+++ www/current/dismiss4.0.php
@@ -0,0 +1,1 @@
+{"response":"success","res":"0x7ce82153","msg":"<p>That's it... Over to you.</p>"}
---
+++ www/current/getMSG.php
@@ -0,0 +1,1 @@
+{"response":"success","html":"<!-- h2>huh?!? <\/h2 -->","uid":"2312ec6cb578de2da7c761c7c4e4618a"}
